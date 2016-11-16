5ebcc16fb9536a75fce43a370809c84206ed981b
Cassie Stuurman <cassiestuurman@gmail.com>
2016-11-16 10:13:31 -0600

    creating user prompt function

--- heatflow2D.cc
+++ heatflow2D.cc
@@ -8,21 +8,29 @@ class heatflow
 {
 public:
   int width, height;
+  void getInputs;
   void createRectangle(int,int);
-
 };
+
+// prompting the user for inputs
+void heatflow :: getInputs()
+{
+  cout << "Please enter the desired width:"
+    cin >> heatflow :: width;
+}
 
 // creating the rectangle for heat flow
 void heatflow :: createRectangle(int dx, int dy)
 {
-
+  
 
 };
 
 
 int main()
 {
-
+  // prompt user for input geometry
+  heatflow :: getInputs;
   return 0;
 }
   
