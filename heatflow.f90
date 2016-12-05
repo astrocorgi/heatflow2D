3a4b02082b0004cebddff0b615521346567af6fb
Cassie Stuurman <cassiestuurman@gmail.com>
2016-12-05 17:12:33 -0600

    I/O+

--- heatflow.f90
+++ heatflow.f90
@@ -1,11 +1,8 @@
 ! This is the module for import into the main heatflow2D program
 module HeatFlow
   implicit none
-
   
 contains
-
-
   
   subroutine readInput(input,output,freq) !./myproject input.dat output_freq output.dat
     !This subroutine reads the inputs from the command line and assigns them variable names
@@ -19,11 +16,9 @@ contains
     !print *, 'the input file is ', input
     !print *, 'the output frequency is ', freq
     !print *, 'the output file will be ', output
+
   end subroutine readInput
 
-
-
-  
   
   subroutine openFile(input,size_x,size_y,alpha,num_timesteps,holds,num_holds)
     !This subroutine opens the input .dat file and assigns it variables in memory
@@ -37,6 +32,10 @@ contains
     open (1,file=input, status='old',action='read')
     read(1,*) size_x, size_y, alpha, num_timesteps
     
+    !convert sizes considering Fortran indexing 
+    size_x = size_x + 1
+    size_y = size_y + 1
+    
     print *, 'x length:', size_x
     print *, 'y_length:', size_y
     print *, 'alpha:', alpha
@@ -44,20 +43,20 @@ contains
     print *, ''
 
     ncol = 4
-    nrow = size_x
+    nrow = size_y 
     k=1
-    allocate(holds(nrow,ncol))
+    allocate(holds(nrow,ncol)) ! clumsy
 
     !Reading unknown length matrix, from ISP slides
     do
-       read(1,*,iostat=ios) holds(k,:)
+       read(1,*,IOSTAT=ios) holds(k,:)
        if (is_iostat_end(ios)) then
           write (*,'(a,i2)') &
                'End of file reached: row= ', k-1 !-1 because first line is read above
           exit
        else if (ios /= 0) then
-          write (*,'(a,i2)') &
-               'Problems reading line', k-1
+          write (*,'(a,i2)') !&
+               !'Problems reading line', k-1
           exit
        endif
        write(*,*) holds(k,:)
@@ -65,41 +64,201 @@ contains
     enddo
 
     num_holds = k-1
-    holds = holds(1:num_holds,:) !I hope this write over holds to make it short
+    !print *, holds
+    print *, holds(1,1)
+    print *, holds(1,2)
+    print *, holds(2,1)
+    !holds = holds(1:num_holds,:) !I hope this write over holds to make it short
     
     close (1)
     !size_x size_y alpha num_timesteps
     !x_pos y_pos temp hold
     !x_pos y_pos temp hold
     !...
-
     
   end subroutine openFile
 
 
+  subroutine readHolds(holds,num_holds,holds_real)
+    !Right now it's a character matrix and I don't like that :(
+    implicit none
+    integer :: i,j,num_holds, chartest,stat
+    real, dimension(:,:), allocatable :: holds_real
+    character(len=16), dimension(4,num_holds) :: holds
 
-  subroutine readHolds(holds,num_holds)
-    !Right now it's a character matrix and I don't like that :( 
-    integer :: i,j,num_holds
-    character(len=16), dimension(num_holds,4) :: holds
-
+    allocate(holds_real(4,num_holds))
     
-    do j = 1,4
-       do i = 1,num_holds
+    !This loop goes through every element in the input matrix "holds". It looks for *'s, turns them to -999. It converts all characters to integers and adds +1 to the xpos and ypos (since they are in C notation)
+    print *, num_holds
+    do i = 1,4
+       do j = 1,num_holds
+          print *, holds(i,j)
           !check if this is a *
-          if holds(j,i) == '*' then
-          !change to -999
-          holds(i,j) = '-999'
+          if (holds(i,j) == '*') then
+          !change to -999 
+             holds(i,j) = '-999'
+             holds_real(i,j) = -999
+          else
+             !change the character to real
+             read(holds(i,j),*,iostat=stat) holds_real
+             
+             !add +1 to all xposition and yposition values that aren't *'s
+             if (j == 1 .OR. j == 2) then
+                holds_real(i,j) = holds_real(i,j) + 1
+             endif
+             
           endif
        enddo
+       !write(*,*) holds_real(i,:)
     enddo
-
-    !change the character matrix to integer
-
-    !add +1 to all xpos ypos values (they are in C notation)
-
     
   end subroutine readHolds
+
+  
+  subroutine heatPropagation(output,freq,size_x,size_y,alpha,num_timesteps,holds_real,num_holds,heatmat)
+    !here we apply the heat equation to each element in the given rectangle
+    character(len=64) :: output, freq
+    integer :: size_x, size_y, num_timesteps, num_holds, y, x, k, t, hold_check
+    integer :: interior, top, bottom, ls, rs, tl, tr, bl, br, corner
+    real, dimension(num_holds,4) :: holds_real
+    real :: alpha
+    real, dimension(:,:,:), allocatable :: heatmat
+
+    allocate(heatmat(size_y,size_x,num_timesteps)) !change to mod(num_timesteps,output) later
+    hold_check = 0
+    interior = 0
+    top = 0
+    bottom = 0
+    ls = 0
+    rs = 0
+
+    !STILL NEED TO INCLUDE *  * WILDCARD HANDLING
+    
+    do t=1,num_timesteps
+       do y = 1,size_y ! for each row
+          do x=1,size_x ! for each column
+             ! check if this position is a hold
+             do k=1,num_holds
+                if ((x==holds_real(k,1)) .AND. (y==holds_real(k,2)) .AND. (holds_real(k,4)==1)) then
+                   heatmat(y,x,t) = holds_real(k,3)
+                   hold_check = 1
+                elseif ((y == holds_real(k,2)) .AND. (holds_real(k,1) == -999)) then
+                   !it's in a hold column, set to hold value
+                   heatmat(y,x,t) = holds_real(k,3)
+                elseif ((x == holds_real(k,1)) .AND. (holds_real(k,2) == -999) .AND. (holds_real(k,4)==1)) then
+                   !it's in a hold row, set to hold value
+                   heatmat(y,x,t) = holds_real(k,3)
+                endif
+             enddo ! for each hold
+             
+             !now determine if it is interior, top, LS, RS, or bottom. Flag accordingly.
+             if (hold_check==0) then
+                if (y == 1) then
+                   if (x==1) then
+                      tl = 1 !top left
+                      corner = 1
+                   elseif (x==size_x) then
+                      tr = 1 !top right
+                      corner = 1
+                   else 
+                      ls = 1
+                   endif
+                elseif (y == size_y) then
+                   if (x ==1) then
+                      bl = 1 ! bottom left
+                      corner = 1
+                   elseif (x==size_x) then
+                      br = 1 ! bottom right
+                      corner = 1
+                   else
+                      rs = 1
+                   endif
+                elseif (x == 1) then
+                   top = 1
+                elseif (x == size_x) then
+                   bottom = 1
+                else
+                   interior = 1
+                endif !determining location flag
+             endif !hold check
+
+             ! now apply the correct heat solution to the element
+             ! CORNER ELEMENTS
+             if (corner==1) then
+                if (tl==1) then
+                   !function without x-1, y-1
+                   !heatmat(y,x,t) =  u(x,y,t-1)  &
+                    !       + u(x+1,y,t-1)+ u(x,y+1,t-1)    &
+                     !      +u(x+1,y+1,t-1)-8*u(x,y,t-1))
+                elseif (tr==1) then
+                   !function without x+1, y-1
+                   
+                   !heatmat(y,x,t) = u(x,y,t-1)  &
+                    !    +u(x-1,y,t-1)+u(x-1,y+1,t-1)+ u(x,y+1,t-1)    &
+                     !   -8*u(x,y,t-1))
+                elseif  (bl==1) then
+                   !function without x-1, y+1
+                   
+                   !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x,y-1,t-1)+ u(x+1,y-1,t-1) &
+                    !    + u(x+1,y,t-1)   &
+                     !   -8*u(x,y,t-1))
+                elseif (br==1) then
+                   !function without y+1, x+1
+                   !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x-1,y-1,t-1)+u(x,y-1,t-1) &
+                    !    +u(x-1,y,t-1)    &
+                     !   -8*u(x,y,t-1))
+                endif
+             else !(corner /= 1), its an edge or interior
+
+                   if (interior==1) then
+                
+                      !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x-1,y-1,t-1)+u(x,y-1,t-1)+ u(x+1,y-1,t-1) &
+                       !    +u(x-1,y,t-1)+ u(x+1,y,t-1)+u(x-1,y+1,t-1)+ u(x,y+1,t-1)    &
+                        !   +u(x+1,y+1,t-1)-8*u(x,y,t-1))
+                
+                   elseif (ls==1) then
+                      !same equation with x-1 terms removed
+                      
+                      !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x,y-1,t-1)+ u(x+1,y-1,t-1) &
+                       !    + u(x+1,y,t-1)+u(x,y+1,t-1)    &
+                        !   +u(x+1,y+1,t-1)-8*u(x,y,t-1))
+                
+                   elseif (rs==1) then
+                      !same equation with x+1 terms removed
+                      !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x-1,y-1,t-1)+u(x,y-1,t-1) &
+                       !    +u(x-1,y,t-1)+u(x-1,y+1,t-1)+ u(x,y+1,t-1)    &
+                        !   -8*u(x,y,t-1))
+
+                   elseif (bottom==1) then
+                      !same equation with y+1 removed
+                   
+                      !heatmat(y,x,t) = u(x,y,t-1) + a*(u(x-1,y-1,t-1)+u(x,y-1,t-1)+ u(x+1,y-1,t-1) &
+                       !    +u(x-1,y,t-1)+ u(x+1,y,t-1)+u(x-1,y+1,t-1)+ u(x,y+1,t-1)    &
+                        !   +u(x+1,y+1,t-1)-8*u(x,y,t-1))
+
+                   elseif (top==1) then
+                      !same equation with y-1 removed
+                      !heatmat(y,x,t) = u(x,y,t-1)  &
+                       !    +u(x-1,y,t-1)+ u(x+1,y,t-1)+u(x-1,y+1,t-1)+ u(x,y+1,t-1)    &
+                        !   +u(x+1,y+1,t-1)-8*u(x,y,t-1))
+                   endif !interior == 1
+                endif
+          enddo ! for each column
+          hold_check = 0 !reset to zero after every element
+          interior = 0
+          top = 0
+          bottom = 0
+          ls = 0
+          rs = 0
+          tl = 0
+          tr = 0
+          bl = 0
+          br = 0
+          corner = 0
+       enddo ! for each row
+    enddo ! for each time step
+    
+  end subroutine heatPropagation
 
   
 end module HeatFlow
--- heatflow2D.f90
+++ heatflow2D.f90
@@ -7,12 +7,18 @@ program HeatCalc
 
   character(len=64) :: input, output, freq
   character(len=16), dimension(:,:), allocatable :: holds
-  integer :: size_x, size_y, num_timesteps
+  integer :: size_x, size_y, num_timesteps, num_holds
   real :: alpha
+  real, dimension(:,:), allocatable :: holds_real
+  real, dimension(:,:,:), allocatable :: heatmat
   
   call readInput(input,output,freq)
 
-  call openFile(input,size_x,size_y,alpha,num_timesteps,holds)
+  call openFile(input,size_x,size_y,alpha,num_timesteps,holds,num_holds)
+
+  call readHolds(holds,num_holds,holds_real)
+
+  call heatPropagation(output,freq,size_x,size_y,alpha,num_timesteps,holds_real,num_holds,heatmat)
   
 end program HeatCalc
 
